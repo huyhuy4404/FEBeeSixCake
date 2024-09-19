401b27174c2a866064c8ac9edb7bdeb4afb0c3fc
huyhuy4404 <hhuydz44@gmail.com>
2024-09-19 10:42:51 +0700

    Initial commit

---
+++ src/main/java/com/poly/febeesixcake/FeBeeSixCakeApplication.java
@@ -0,0 +1,13 @@
+package com.poly.febeesixcake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeBeeSixCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeBeeSixCakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/febeesixcake/FeBeeSixCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.febeesixcake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeBeeSixCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
