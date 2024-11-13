f95d7a8ea10afde9d39d807bea6bbf31051280b2
NGUYEN NHAT PHAT <pn02705@gmail.com>
2024-11-13 19:20:54 +0700

    update_information_address_product

--- src/main/resources/templates/assets/js/Information.js
+++ src/main/resources/templates/assets/js/Information.js
@@ -1,9 +1,5 @@
 var app = angular.module('myApp', []);
-<<<<<<< HEAD
 app.controller("CheckLogin", function ($scope, $http, $window) {
-=======
-app.controller("CheckLogin", function ($scope, $http, $window, $timeout) {
->>>>>>> 25a6119be2d6279d4d3c00b3b5cf72d75d00ec3f
     // Khởi tạo thông tin người dùng và trạng thái đăng nhập
     $scope.isLoggedIn = false;
     $scope.user = {
@@ -84,10 +80,7 @@ app.controller("CheckLogin", function ($scope, $http, $window, $timeout) {
       $window.location.href = "login.html"; // Chuyển về trang đăng nhập sau khi đăng xuất
     };
   });
-<<<<<<< HEAD
   
-=======
->>>>>>> 25a6119be2d6279d4d3c00b3b5cf72d75d00ec3f
 app.controller('Controller', function($scope, $http) {
     // Hàm để lấy thông tin tài khoản đã đăng nhập từ localStorage
     $scope.getUserInfo = function() {
